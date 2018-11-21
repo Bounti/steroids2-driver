72b9f028b3b67f5d86eead2dbbbb6f0c46905fa3
Corteggiani <nassim.corteggiani@maximintegrated.com>
2018-11-21 10:56:28 +0100

    Fix scan packet size to work with the Inception's debugger (multiple of 32 bits as input and twice the input lenght in output)

--- lib/api.cpp
+++ lib/api.cpp
@@ -7,33 +7,35 @@
 #include <termcolor.hpp>
 #include <unistd.h>
 
-device* init() {
+device *init() {
   device *io = new device(0x04B4, 0x00F1, 0);
   io->init();
 
   return io;
 }
 
-void reset(device* io) {
+void reset(device *io) {
   auto crafted_packet = crafter::craft(c_reset, 0, string());
   io->send(crafted_packet.first, crafted_packet.second);
 }
 
-uint8_t* read(device* io, uint32_t address, uint32_t size) {
+// XXX: WARNING !!
+// Fow now only multiple of 4byte (32bits) is accepted as size argument
+uint8_t *read(device *io, uint32_t address, uint32_t size) {
   std::string payload = "";
 
-  uint8_t* data = new uint8_t[size]();
+  uint8_t *data = new uint8_t[size]();
 
   auto crafted_packet = crafter::craft(c_read, address, payload);
   io->send(crafted_packet.first, crafted_packet.second);
-  io->receive(data, size);
+
+  io->receive(data, size * 2);
 
   return data;
 }
 
-
-void write(device* io, uint32_t address, uint32_t size, uint8_t* data) {
-  std::string payload((char*)data, size);
+void write(device *io, uint32_t address, uint32_t size, uint8_t *data) {
+  std::string payload((char *)data, size);
 
   auto crafted_packet = crafter::craft(c_write, address, payload);
   io->send(crafted_packet.first, crafted_packet.second);
--- tools/scan_test/main.cpp
+++ tools/scan_test/main.cpp
@@ -11,35 +11,18 @@ int main(int argc, char *argv[]) {
   device *io = new device(0x04B4, 0x00F1, 0);
   io->init();
 
-  // device *irq = new device(0x04b4, 0x00f1, 0, 0x02, 0x82);
-  // irq->init();
+  uint32_t data_dwords[] = {0xABABABAB, 0xF0F0F0F0};
+  uint32_t data_dwords_size = sizeof(data_dwords);
 
-  uint32_t data_dwords[] = {0xABABABAB, 0xABABABAB, 0xABABABAB, 0xABABABAB};
-  uint8_t reception[16] = {0};
-  std::string payload((char *)data_dwords, 16);
+  uint8_t *reception = new uint8_t[data_dwords_size]();
+  std::string payload((char *)data_dwords, data_dwords_size);
 
-  //auto crafted_packet = crafter::craft(c_reset, 0, string());
-  //io->send(crafted_packet.first, crafted_packet.second);
-
-  auto crafted_packet = crafter::craft(c_scan, 0x4, payload);
+  auto crafted_packet = crafter::craft(c_scan, data_dwords_size / 4, payload);
   io->send(crafted_packet.first, crafted_packet.second);
-  io->receive(reception, 16);
 
-  //crafted_packet = crafter::craft(c_reset, 0, string());
-  //io->send(crafted_packet.first, crafted_packet.second);
-
-  // for (int i = 0; i < 10; i++) {
-  //   cout << termcolor::blue << "wait for next cmd " << i << endl;
-  //   usleep(3000000);
-  //   io->send((uint8_t *)&data_dwords[0], 4);
-  // }
-
-  // crafted_packet = crafter::craft(c_write, 0x20000000, string());
-  // io->send(crafted_packet.first, crafted_packet.second);
+  // Reception is scan payload * 2 because the Inception
+  // debugger return 64bits length packet only
+  io->receive(reception, data_dwords_size * 2);
 
   io->close();
-  // irq->close();
-
-  // crafted_packet = crafter::craft(scan, data);
-  // device_jtag->send(crafted_packet.first, crafted_packet.second);
 }
